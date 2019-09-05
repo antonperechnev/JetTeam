e592ea38ca158a89df228dcd30dc45b02fcab5c2
Anton Perechnev <perechnev@stips.io>
2019-09-05 18:32:01 +0500

    another old

--- analyze.py
+++ analyze.py
@@ -6,10 +6,13 @@ import json
 import numpy as np
 
 engine = create_engine('postgresql://postgres:Anton1995@localhost/allerts')
-df = pd.read_csv('vh.csv')
+df = pd.read_csv('new_data1.csv', parse_dates=['timestamp'])
+# print(type(df['timestamp'][0]))
 # df['change liters'] = df['liters'].diff().fillna(0).shift(-1).fillna(0)
-df['unix time'] = pd.to_datetime(df['timestamp'])
-df.to_csv('new_data.csv')
+# df['timestamp'] = df['timestamp'].astype('datetime64[ns]')
+# разница между временами
+# df['time delta'] = pd.to_datetime(df['unix time']).diff().fillna(0).shift(-1).fillna(0)
+# df.to_csv('new_data1.csv', date_format='%Y-%m-%d %H:%M:%S')
 
 
 def create_ids_json():
@@ -19,7 +22,7 @@ def create_ids_json():
     return len(unique_id)
 
 
-def action_handler(data: list, consumption: bool = 0):
+def action_handler(data: list, consumption=0):
     temp_df = pd.DataFrame(data)
     start_period_date = temp_df['time'].min()
     min_date = start_period_date.timestamp()
@@ -36,42 +39,61 @@ def action_handler(data: list, consumption: bool = 0):
     return {'start period': start_period_date, 'duration': duration, 'liters delta': liters_delta, 'action': action}
 
 
+def list_to_dictlist(raw_data: list, consumption=0):
+    prod = []
+    temp = []
+    for chunk in raw_data:
+        if chunk == '|':
+            data = action_handler(temp)
+            if consumption:
+                data = action_handler(temp, consumption=1)
+            prod.append(data)
+            temp = []
+            continue
+        temp.append(chunk)
+    return prod
+
+
 def one_ts_analyze(c_id):
     for_df = []
-    temp = []
-    one_car = df.loc[df['id'] == c_id]
-    one_car['change liters'] = one_car['liters'].diff().fillna(0).shift(-1).fillna(0)
-    sign = one_car['change sign'][0]
-    for ind, row in one_car.iterrows():
-        # для отбрасывания колебаний датчика
-        if row['change sign'] != sign and len(temp) < 2:
-            sign = row['change sign']
-            continue
-        temp.append({'time': pd.to_datetime(row['unix time']), 'liters': row['liters']})
-        if row['change sign'] != sign and len(temp) > 1:
-            # call func
-            if sign == 1.0:
-                # refill
-                data_refill = action_handler(temp)
-                for_df.append(data_refill)
-                temp = []
-                sign = row['change sign']
-            elif sign == -1.0:
-                # consumption
-                data_cons = action_handler(temp, consumption=True)
-                for_df.append(data_cons)
-                temp = []
-                sign = row['change sign']
-            elif sign == 0.0:
-                temp = []
-                sign = row['change sign']
+    consum = []
+    refill = []
+    rest = []
 
+    one_car = df.loc[df['id'] == c_id]
+    one_car['time delta'] = one_car['timestamp'].diff().fillna(0).shift(-1).fillna(0)
+    one_car['change liters'] = one_car['liters'].diff().fillna(0).shift(-1).fillna(0)
+
+    liters = one_car['liters'][0]
+    for ind, row in one_car.iterrows():
+        if liters == 0:
+            liters = row['liters']
+            rest.append({'time': row['timestamp'], 'liters': row['liters']})
+            continue
+        if -2 <= row['change liters'] <= 2:
+            if refill:
+                refill.append('|')
+            consum.append({'time': row['timestamp'], 'liters': row['liters']})
+            continue
+        if 30 > row['change liters'] > 2:
+            if consum:
+                consum.append('|')
+            refill.append({'time': row['timestamp'], 'liters': row['liters']})
+            continue
+        if abs(row['change liters']) >= 30 and row['time delta'] > pd.Timedelta(minutes=15):
+            rest.append('|')
+            rest.append({'time': row['timestamp'], 'liters': row['liters']})
+            continue
+
+    # for_df.extend(list_to_dictlist(consum, consumption=1))
+    # for_df.extend(list_to_dictlist(refill))
+    for_df.extend(list_to_dictlist(consum, consumption=1))
     pd.DataFrame(for_df).to_csv('final1.csv')
     return "OK"
     # return (pd.to_datetime(row['timestamp']).timestamp() - pd.to_datetime(st_time).timestamp())/60
 
 
-# print(one_ts_analyze("d28378aec6bd1214b87047f2af506f70"))
+print(one_ts_analyze("d28378aec6bd1214b87047f2af506f70"))
 
 
 # return (pd.to_datetime(row['timestamp']).timestamp() - pd.to_datetime(st_time).timestamp())/60
