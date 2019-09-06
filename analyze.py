45a5c3c6c167f3e5f9f629733355040dd4520ac5
Anton Perechnev <perechnev@stips.io>
2019-09-06 16:14:47 +0500

    threading

--- analyze.py
+++ analyze.py
@@ -4,6 +4,9 @@ from sqlalchemy import create_engine
 from datetime import datetime
 import json
 import numpy as np
+from threading import Thread, active_count
+import time
+import queue
 
 engine = create_engine('postgresql://postgres:Anton1995@localhost/allerts')
 
@@ -21,7 +24,7 @@ def create_ids_json():
     return len(unique_id)
 
 
-def action_handler(data: list, consumption=0):
+def action_handler(data: list, c_id, consumption=0):
     temp_df = pd.DataFrame(data)
 
     start_period_date = temp_df['time'].min()
@@ -36,7 +39,8 @@ def action_handler(data: list, consumption=0):
         action = 'consumption'
 
     duration = max_date - min_date
-    return {'start period': start_period_date, 'duration': duration, 'liters delta': liters_delta, 'action': action}
+    return {'start period': start_period_date, 'duration': duration, 'liters delta': liters_delta, 'action': action,
+            'c_id': c_id}
 
 
 # not work
@@ -67,13 +71,13 @@ def one_ts_analyze(c_id):
     for ind, row in one_car.iterrows():
         if -2 <= row['change liters past'] <= 2:
             if refill:
-                for_df.append(action_handler(refill))
+                for_df.append(action_handler(refill, c_id))
                 refill = []
             consum.append({'time': row['timestamp'], 'liters': row['liters']})
         # refill
         if (30 > row['change liters'] > 2) or (30 > row['change liters past'] > 2):
             if len(consum) > 1:
-                for_df.append(action_handler(consum, consumption=1))
+                for_df.append(action_handler(consum, c_id, consumption=1))
                 consum = []
             refill.append({'time': row['timestamp'], 'liters': row['liters']})
         if abs(row['change liters']) >= 30 and row['time delta'] > pd.Timedelta(minutes=15):
@@ -86,19 +90,41 @@ def one_ts_analyze(c_id):
     # return (pd.to_datetime(row['timestamp']).timestamp() - pd.to_datetime(st_time).timestamp())/60
 
 
-def analyze_all():
+with open('unique_id.json') as f:
+    ids_list = json.load(f)
+
+
+q = queue.Queue()
+
+
+def analyze_all(new_queue, st, fin):
     to_df = []
-    with open('unique_id.json') as f:
-        ids_list = json.load(f)
-    for vh_id in ids_list:
+    chunk_ids = ids_list[st:fin]
+    for vh_id in chunk_ids:
         data = one_ts_analyze(vh_id)
         print(vh_id)
         to_df.extend(data)
-    pd.DataFrame(to_df).to_csv('final_new.csv', index=False)
+    new_queue.put(to_df)
+    # pd.DataFrame(to_df).to_csv('final_new.csv', index=False)
 
 
 if __name__ == '__main__':
-    print(analyze_all())
-    # print(one_ts_analyze("d28378aec6bd1214b87047f2af506f70"))
+    l_df = []
+    all_thread = []
+    count = 0
+    offset = round(len(ids_list)/8)
+    for _ in range(8):
 
+        thread = Thread(target=analyze_all, args=(q, count, count + offset))
+
+        thread.start()
+        count += offset
+
+    while thread.is_alive():
+        l_df.append(q.get())
+
+    for thread in all_thread:
+        thread.join()
+
+    pd.DataFrame(l_df).to_csv('final.csv', index=False)
 
