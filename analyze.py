69257a1550e5575fa321ffcc9ec327c211d57d7f
Anton Perechnev <perechnev@stips.io>
2019-09-06 12:41:22 +0500

    work for 1/10

--- analyze.py
+++ analyze.py
@@ -8,18 +8,15 @@ import numpy as np
 engine = create_engine('postgresql://postgres:Anton1995@localhost/allerts')
 
 df = pd.read_csv('vh.csv', parse_dates=['timestamp'], index_col=False)
-# print(df.head())
-# print(type(df['timestamp'][0]))
-# df['change liters'] = df['liters'].diff().fillna(0).shift(-1).fillna(0)
-# df['timestamp'] = df['timestamp'].astype('datetime64[ns]')
+# df[df['id'] == "988c80e0861e328d70d1a9b4860c51f5"].to_csv('one_vh.csv', date_format='%Y-%m-%d %H:%M:%S', index=False)
+
 # разница между временами
 # df['time delta'] = pd.to_datetime(df['unix time']).diff().fillna(0).shift(-1).fillna(0)
-# df[df['id'] == "d28378aec6bd1214b87047f2af506f70"].to_csv('one_vh.csv', date_format='%Y-%m-%d %H:%M:%S', index=False)
 
 
 def create_ids_json():
     unique_id = df['id'].unique().tolist()
-    with open('unique_id', 'w') as f:
+    with open('unique_id.json', 'w') as f:
         json.dump(unique_id, f)
     return len(unique_id)
 
@@ -42,21 +39,7 @@ def action_handler(data: list, consumption=0):
     return {'start period': start_period_date, 'duration': duration, 'liters delta': liters_delta, 'action': action}
 
 
-def list_to_dictlist(raw_data: list, consumption=0):
-    prod = []
-    temp = []
-    for chunk in raw_data:
-        if chunk == '|':
-            data = action_handler(temp)
-            if consumption:
-                data = action_handler(temp, consumption=1)
-            prod.append(data)
-            temp = []
-            continue
-        temp.append(chunk)
-    return prod
-
-
+# not work
 def ts_analyze(c_id):
     one_car = df[df['id'] == c_id]
     one_car['time delta'] = one_car['timestamp'].diff().fillna(0).shift(-1).fillna(0)
@@ -80,13 +63,8 @@ def one_ts_analyze(c_id):
     one_car['change liters'] = one_car['liters'].diff().fillna(0).shift(-1).fillna(0)
     one_car['change liters past'] = one_car['liters'].diff().fillna(0)
 
-    # liters = one_car['liters'][0]
+    # обработка состояния
     for ind, row in one_car.iterrows():
-        '''if liters == 0:
-            liters = row['liters']
-            rest.append({'time': row['timestamp'], 'liters': row['liters']})
-            continue
-        '''
         if -2 <= row['change liters past'] <= 2:
             if refill:
                 for_df.append(action_handler(refill))
@@ -94,9 +72,8 @@ def one_ts_analyze(c_id):
             consum.append({'time': row['timestamp'], 'liters': row['liters']})
         # refill
         if (30 > row['change liters'] > 2) or (30 > row['change liters past'] > 2):
-            if consum:
+            if len(consum) > 1:
                 for_df.append(action_handler(consum, consumption=1))
-                print(consum)
                 consum = []
             refill.append({'time': row['timestamp'], 'liters': row['liters']})
         if abs(row['change liters']) >= 30 and row['time delta'] > pd.Timedelta(minutes=15):
@@ -104,10 +81,24 @@ def one_ts_analyze(c_id):
             rest.append({'time': row['timestamp'], 'liters': row['liters']})
             continue
 
-    pd.DataFrame(for_df).to_csv('final_new.csv', index=False)
-    return "OK"
+    # pd.DataFrame(for_df).to_csv('final_new.csv', index=False)
+    return for_df
     # return (pd.to_datetime(row['timestamp']).timestamp() - pd.to_datetime(st_time).timestamp())/60
 
 
-print(one_ts_analyze("d28378aec6bd1214b87047f2af506f70"))
+def analyze_all():
+    to_df = []
+    with open('unique_id.json') as f:
+        ids_list = json.load(f)
+    for vh_id in ids_list:
+        data = one_ts_analyze(vh_id)
+        print(vh_id)
+        to_df.extend(data)
+    pd.DataFrame(to_df).to_csv('final_new.csv', index=False)
+
+
+if __name__ == '__main__':
+    print(analyze_all())
+    # print(one_ts_analyze("d28378aec6bd1214b87047f2af506f70"))
+
 
